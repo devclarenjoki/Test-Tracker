4e45c28d00a56324e509ad5d410ad3b4f2e5e3e9
clare mburu <mburuclare8@gmail.com>
2022-04-10 07:55:46 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component:TasksComponent},
+  {path: 'about', component:AboutComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
